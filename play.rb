e2b38f01bff284b7c001a0f649b9253a8f0805c5
Farid Asadpour <yani5752000@yahoo.com>
2021-08-16 16:46:00 +0000

    commit

---
+++ play.rb
@@ -0,0 +1,16 @@
+class Players
+  require './player1'
+  require './player2'
+  
+  def initialize(plaeyer1, players2)
+    @player1 = player1
+    @player2 = player2
+    @player1_lives = 3
+    @player2_lives = 3
+  end
+  
+  # def set_name(new)
+  #   @name = new;
+  # end
+
+end
